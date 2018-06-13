e2f10595b0f474209044238ea893baf2ddc3aaa9
Anabel9222 <Anabel9222@anna.ossowska92@gmail.com>
2018-06-13 23:21:00 +0200

    dodano 2 zadania

---
+++ TheProjectEulerLibrary/EulerLibrary2/ProjectEulerLibrary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace TheProjectEulerLibrary
+{
+    public class ProjectEulerLibrary
+    {
+        public static long ClassicMultiplesOf3And5(int limit)
+        {
+            long sum = 0;
+            for (int i = 1; i < limit; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i; // sum = sum + i;
+                }
+            }
+
+            return sum;
+        }
+
+        public static long LinqMultiplesOf3And5(int limit)
+        {
+            var result = Enumerable.Range(0, limit)
+                .Where(i => i % 3 == 0 || i % 5 == 0)
+                .Sum();
+            return result;
+        }
+        public static long Abs (int value)
+        {
+            if ( value < 0){
+
+                value = - value;
+                
+
+            }
+            else if ( value >0)
+            {
+                value = value;
+
+            }
+            else{
+
+                value = 0;
+            }
+            return value;
+        }
+    }
+}
